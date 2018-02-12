c39eb162b64097a6c5657ff5df3811ec4673ed7e
PresentKim <qjrmckd@gmail.com>
2018-02-12 19:24:56 +0900

    받은 데이터를 모든 폼에 대응하여 처리합니다

--- src/RPGGameUI/Main.php
+++ src/RPGGameUI/Main.php
@@ -49,12 +49,11 @@ class Main extends PluginBase implements Listener{
     public function setui(DataPacketReceiveEvent $event){
         $packet = $event->getPacket();
         if ($packet instanceof ModalFormResponsePacket) { // 폼에 대한 응답
+            $responseData = json_decode($packet->formData);
+            if (is_null($responseData)) { // 선택없이 닫힌 경우
+                return; // 아무 작동도 하지않고 중단합니다
+            }
             if ($packet->formId == 2225) { // 메인 폼에 대한 응답
-                $responseData = json_decode($packet->formData);
-                if (is_null($responseData)) { // 선택없이 닫힌 경우
-                    return; // 아무 작동도 하지않고 중단합니다
-                }
-
                 $formPacket = new ModalFormRequestPacket ();
                 if ($responseData) { // button1: 보스전을 선택한 경우
                     $formPacket->formId = 2226;
