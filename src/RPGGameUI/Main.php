7592c336fd58df9a73005bc8339bd4ddd621ab72
BuildLike <33346622+BuildLike@users.noreply.github.com>
2018-02-27 18:45:54 +0900

    대형패치
    RPG상점,방어,보스전메뉴로 돌아가기,메뉴 기능 추가,RPG상점 아이템 구매=판매 추가
    GitHub에서 수정 하였습니다.
    추가 되고 싶은 기능은 이슈로 알려주세요.

--- src/RPGGameUI/Main.php
+++ src/RPGGameUI/Main.php
@@ -32,7 +32,7 @@ class Main extends PluginBase implements Listener{
             $formPacket->formData = json_encode([
               "type"    => "modal",
               "title"   => "§l§d[ §fRPGGameUI §d]§r§f",
-              "content" => "§l§cRPGUI v1.0.0\n§d레벨 : {$this->getConfig()->get('레벨')}",
+              "content" => "§l§cRPGUI Beta2\n§d레벨 : {$this->getConfig()->get('레벨')}",
               "button1" => "§l§c[ §f보스전 §c]§r§f",
               "button2" => "§l§d[ §f메뉴 §d]§r§f",
             ]);
